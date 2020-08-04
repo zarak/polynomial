77eb2b4432881a8b52bf1cea5a093f2aaffd642b
Zarak <7254237+zarak@users.noreply.github.com>
2020-08-04 08:47:45 +0500

    Add additional tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,10 +5,17 @@ import Lib
 main :: IO ()
 main = hspec spec
 
+t1 = Term (Coefficient 1) (Exponent 2)
 p1 = Polynomial []
+p2 = Polynomial [t1]
+p3 = Polynomial [t1, t1]
 
 spec :: Spec
 spec =
-    describe "Polynomial" $ do
+    describe "derivative" $ do
         it "should return an empty list for a null result" $
             derivative p1 `shouldBe` Polynomial []
+        it "should differentiate a list with one term" $
+            derivative p2 `shouldBe` Polynomial [(Coefficient 2, Exponent 1)]
+        it "should differentiate a list with two terms" $
+            derivative p3 `shouldBe` Polynomial [(Coefficient 2,  Exponent 1), (Coefficient 2,  Exponent 1)]
