395e8316bd7c5d8cc51da512a61c69dd671c145e
Zarak <7254237+zarak@users.noreply.github.com>
2020-08-10 17:22:51 +0500

    Update to match submission

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,42 +2,49 @@ module Lib where
 
 import Data.Map
 
+-- Types
+type IntegerTerm = Term Integer Integer
 
-newtype Exponent e = Exponent e
+newtype Exponent e =
+    Exponent e
     deriving (Eq, Ord, Show)
 
-newtype Coefficient c = Coefficient c
+newtype Coefficient c =
+    Coefficient c
     deriving (Eq, Ord, Show)
 
 instance Functor Coefficient where
     fmap f (Coefficient a) = Coefficient (f a)
 
-data Term e c = Term (Exponent e) (Coefficient c)
-          deriving (Eq, Ord, Show)
+instance Applicative Coefficient where
+    pure = Coefficient
+    Coefficient f <*> Coefficient a = Coefficient (f a)
 
-type IntegerTerm = Term Integer Integer
+data Term e c =
+    Term (Exponent e) (Coefficient c)
+    deriving (Eq, Ord, Show)
 
 data Polynomial = Polynomial [IntegerTerm]
     deriving (Eq, Show)
 
 
+-- Functions
+--
+-- Version 1 using the Polynomial data type
 derivative :: Polynomial -> Polynomial
 derivative (Polynomial terms) = Polynomial $ fmap derivative' terms
-
 
 derivative' :: IntegerTerm -> IntegerTerm
 derivative' (Term (Exponent e) (Coefficient c)) =
     Term (Exponent (e - 1)) (Coefficient (e * c))
 
-
+-- Version 2 using a list of coefficients
 -- Assume the index represents the exponent n
 listDeriv :: Num a => [Coefficient a] -> [Coefficient a]
 listDeriv [] = []
-listDeriv (x:xs) = auxDeriv xs 1
+listDeriv (x:xs) = listDeriv' xs 1
 
-
-auxDeriv :: Num a => [Coefficient a] -> a -> [Coefficient a]
-auxDeriv [] _ = []
-auxDeriv (c:cs) n =
-    case c of
-      Coefficient x -> Coefficient (x * n) : auxDeriv cs (n+1)
+listDeriv' :: Num a => [Coefficient a] -> a -> [Coefficient a]
+listDeriv' [] _ = []
+listDeriv' (c:cs) n =
+    ((*) <$> c <*> Coefficient n) : listDeriv' cs (n+1)
